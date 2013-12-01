db4eaf0bc6a167e71e9d2a623ce5e50948f314c1
Thorsten Hoeger <thorsten.hoeger@hoegernet.com>
2013-12-01 17:17:12 +0100

    format Header

--- src/main/java/org/restdoc/jersey/server/RestDocFeature.java
+++ src/main/java/org/restdoc/jersey/server/RestDocFeature.java
@@ -3,18 +3,22 @@ package org.restdoc.jersey.server;
 /**
  * restdoc-server-jersey
  * 
- * Based on org.restdoc.cxf.RestDocFeature by thoeger, restdoc.org This is a minimal stripped-down version tested with Winery
+ * Based on org.restdoc.cxf.RestDocFeature by thoeger, restdoc.org 
+ * This is a minimal stripped-down version tested with Winery
  * 
  * Copyright (C) 2013 Oliver Kopp
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may
- * obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
- * and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import javax.ws.rs.core.Response;
